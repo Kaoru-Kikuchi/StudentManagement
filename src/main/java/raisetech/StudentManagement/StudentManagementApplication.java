c3b1ce1b19fb738b743e3cf183a5fb48c925faa7
Kaoru Kikuchi <sora0710kk@outlook.jp>
2025-07-28 10:25:53 +0900

    第10回スタンダード演習課題

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -4,11 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,40 +15,17 @@ public class StudentManagementApplication {
   @Autowired
   private StudentRepository repository;
 
-  private String name = "Enami Kouji";
-  private String age = "37";
-
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/student")
-  public String getStudent(@RequestParam("name") String name) {
-    Student student = repository.searchByName(name);
-    if (student == null) {
-      return name + "は登録されていません。";
-    }
-    return student.getName() + " " + student.getAge() + "歳";
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.search();
   }
 
-  @GetMapping("/students")
-  public List<Student> searchByAllName() {
-    return repository.searchByAllName();
-  }
-
-  @PostMapping("/student")
-  public void registerStudent(String name, int age) {
-    repository.registerStudent(name, age);
-  }
-
-
-  @PatchMapping("/student")
-  public void updateStudent(String name, int age) {
-    repository.updateStudent(name, age);
-  }
-
-  @DeleteMapping("/student")
-  public void deleteStudent(String name) {
-    repository.deleteStudent(name);
+  @GetMapping("/studentCourseList")
+  public List<StudentCourse> getStudentCourseList() {
+    return  repository.searchCourse();
   }
 }
