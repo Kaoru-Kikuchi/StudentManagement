7fd278f19c1369a66eea3164bb0a4279963bd5b1
Kaoru Kikuchi <sora0710kk@outlook.jp>
2025-08-05 20:31:02 +0900

    第12回演習課題

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,35 +1,45 @@
 package raisetech.StudentManagement.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
 @RestController
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service,StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList().stream()
-        .filter(s -> s.getAge() >= 30 && s.getAge() <= 39)
-        .toList();
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentCourse> studentsCourses = service.searchStudentsCourseList();
+
+    return converter.convertStudentDetails(students, studentsCourses);
+
   }
 
+
+
   @GetMapping("/studentCourseList")
-  public List<StudentCourse> getStudentsCourseList() {
-    return service.searchStudentsCourseList().stream()
-        .filter(c -> "Java基礎".equals(c.getCourseName()))
-        .toList();
+    public List<StudentCourse> getStudentsCourseList () {
+      return service.searchStudentsCourseList().stream()
+          .filter(c -> "Java基礎".equals(c.getCourseName()))
+          .toList();
+    }
   }
-}
 
