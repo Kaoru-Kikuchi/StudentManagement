84cfc3e3043e23a4251901c18e1746dbba8424d0
Kaoru Kikuchi <sora0710kk@outlook.jp>
2026-07-11 11:08:31 +0900

    Read処理のServiceとController部分を実装

--- src/main/java/raisetech/student/management/StudentManagementApplication.java
+++ src/main/java/raisetech/student/management/StudentManagementApplication.java
@@ -1,36 +1,12 @@
 package raisetech.student.management;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class StudentManagementApplication {
-
-	@Autowired
-	private StudentRepository repository;
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
-	}
-
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		return repository.search();
-	}
-
-	@GetMapping("/studentCourseList")
-	public List<StudentCourse> getStudentCourseList() {
-		return repository.searchStudentCourseList();
 	}
 }
---
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -0,0 +1,27 @@
+package raisetech.student.management.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentCourse;
+
+/**
+ * 受講生情報を扱うリポジトリです。
+ *
+ * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです。
+ */
+
+@Mapper
+public interface StudentRepository {
+
+  /**
+   *全件検索します。
+   * @return　全件検索した受講生情報の一覧
+   */
+  @Select("SELECT * FROM students")
+  List<Student> search();
+
+  @Select("SELECT * FROM students_courses")
+  List<StudentCourse> searchStudentCourseList();
+}
